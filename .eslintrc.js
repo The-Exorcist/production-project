0933e62515c85baeba0416b45693535a0fe5ed75
Konstantin <constantine.main@gmail.com>
2023-05-28 00:45:53 +0300

    Added stylelint, i18n plugin, fixed styles

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,8 @@ module.exports = {
   },
   extends: [
     'plugin:react/recommended',
-    'airbnb'
+    'airbnb',
+    'plugin:i18next/recommended'
   ],
   overrides: [
   ],
@@ -16,7 +17,8 @@ module.exports = {
     sourceType: 'module'
   },
   plugins: [
-    'react'
+    'react',
+    'i18next'
   ],
   rules: {
     "react/jsx-indent": [2, 4],
@@ -36,7 +38,8 @@ module.exports = {
     'no-shadow': 'off',
     'import/extensions': 'off',
     'import/no-extraneous-dependencies': 'off',
-    'no-underscore-dangle': 'off'
+    'no-underscore-dangle': 'off',
+    "i18next/no-literal-string": ['error', {markupOnly: true}]
   },
   globals: {
     '__IS_DEV__': true
