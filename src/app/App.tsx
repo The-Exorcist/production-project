b540273319c55eb3e64a4305aa008db78b2c8870
Konstantin <constantine.main@gmail.com>
2023-06-04 19:46:00 +0300

    Defunct routes. Added pageloader

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,9 @@
-import React, { Suspense } from 'react';
+import { useTheme } from 'app/providers/ThemeProvider';
+import { AppRouter } from 'app/providers/router';
+import { Suspense } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Sidebar } from 'widget/Sidebar';
 import { Navbar } from 'widget/navbar';
-import { useTheme } from 'app/providers/ThemeProvider';
-import { AppRouter } from 'app/providers/router';
 import './styles/index.scss';
 
 function App() {
