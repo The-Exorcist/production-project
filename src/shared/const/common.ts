da28247672158cd4e4137ea7e8e318500049924a
Konstantin <constantine.main@gmail.com>
2024-02-02 17:11:51 +0300

    Added profile, optimization render

---
+++ src/shared/const/common.ts
@@ -0,0 +1,13 @@
+export enum Currency {
+    RUB = 'RUB',
+    EUR = 'EUR',
+    USD = 'USD',
+}
+
+export enum Country {
+    Russia = 'Russia',
+    Ukrain = 'Ukrain',
+    Belarus = 'Belarus',
+    Kazahstan = 'Kazahstan',
+    Armenia = 'Armenia',
+}
