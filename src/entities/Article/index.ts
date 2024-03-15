17d904da036844b239457aa43b28fc9cf2071c6b
Konstantin <constantine.main@gmail.com>
2024-03-15 15:21:27 +0300

    Added skeleton, code, article details card

---
+++ src/entities/Article/index.ts
@@ -0,0 +1,6 @@
+export { ArticleDetailsSchema } from './model/types/ArticleDetailsSchema';
+export {
+    AritcleDetails,
+} from './ui/AritcleDetails/AritcleDetails';
+
+export type { Article } from './model/types/article';
